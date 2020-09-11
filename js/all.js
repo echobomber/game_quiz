f9c9855e28317c400b75d97627c07f6051f7f294
echobomber <jordan990301@g-mail.nsysu.edu.tw>
2020-09-11 12:15:29 +0800

    解決破版

--- js/all.js
+++ js/all.js
@@ -1,21 +1,19 @@
 $(document).ready(function(){
     $('.Quiz').hide();
-
-    $('.Quiz-Start a').click(function(e) {
+    $('.Quiz-Start a').on('click', function(e){
         e.preventDefault();
         $(this).parent().fadeOut(1000);
         $(this).parent().next().fadeIn(1000);
     });
-
     $('.select-Btn').each(function(){
-        $(this).click(function(e){
+        $(this).on('click', function(e){
             e.preventDefault();
             $('.select-Btn').removeClass('btn-light').addClass('btn-outline-light');
             $(this).removeClass('btn-outline-light').addClass('btn-light');
         });
     });
-
-    $('.confirm-Btn').click(function(){
+    $('.confirm-Btn').on('click', function(e){
+        e.preventDefault();
         var target = $('.select-Btn.btn-light');
         console.log(target.text());
 
@@ -24,33 +22,32 @@ $(document).ready(function(){
             target.removeClass('btn-light').addClass('btn-success');
             Quiz.delay(500).fadeOut(1000);
             Quiz.next().delay(500).fadeIn(1000);
-            target.removeClass('btn-light').addClass('btn-outline-light');
         }else if(target.data('ans') == '0'){
             target.removeClass('btn-light').addClass('btn-danger');
             Quiz.delay(500).fadeOut(1000);
             $('#Lose').delay(500).fadeIn(1000);
-            target.removeClass('btn-light').addClass(' btn-outline-light');
         }else{
             console.log('你還沒選喔');
         }
-    });
-    $('#Lose a').click(function(){
-        var target1 = $('.select-Btn.btn-success, .select-Btn.btn-danger');
+    })
+    $('#Lose a').on('click', function(e){
+        e.preventDefault();
+        var target1 = $('.select-Btn.btn-success');
         var target2 = $('.select-Btn.btn-danger');
-        target1.removeClass('btn-success');
-        target1.removeClass('btn-danger');
+        target1.removeClass('btn-success').addClass('btn-outline-light');;
+        target2.removeClass('btn-danger').addClass('btn-outline-light');;
         $('#Lose').fadeOut(1000);
         $('.Quiz-Start').fadeIn(1000);
     })
-    $('#End a').click(function(){
-        var target1 = $('.select-Btn.btn-success, .select-Btn.btn-danger');
+    $('#End a').on('click', function(e){
+        e.preventDefault();
+        var target1 = $('.select-Btn.btn-success');
         var target2 = $('.select-Btn.btn-danger');
-        target1.removeClass('btn-success');
-        target1.removeClass('btn-danger');
+        target1.removeClass('btn-success').addClass('btn-outline-light');;
+        target2.removeClass('btn-danger').addClass('btn-outline-light');;
         $('#End').fadeOut(1000);
         $('.Quiz-Start').fadeIn(1000);
     })
-
 });
 
 
