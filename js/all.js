c24ebf3b1686780378093503f278cc1383b151fd
echobomber <jordan990301@g-mail.nsysu.edu.tw>
2020-11-19 01:09:12 +0800

    test

--- js/all.js
+++ js/all.js
@@ -26,8 +26,14 @@ $(document).ready(function(){
             target.removeClass('btn-light').addClass('btn-danger');
             Quiz.delay(500).fadeOut(1000);
             $('#Lose').delay(500).fadeIn(1000);
-        }else{
-            console.log('你還沒選喔');
+        }else if(target.data('ans') == '2'){
+            target.removeClass('btn-light').addClass('btn-success');
+            Quiz.delay(500).fadeOut(1000);
+            $('#Win2').delay(500).fadeIn(1000);
+        }else if(target.data('ans') == '3'){
+            target.removeClass('btn-light').addClass('btn-success');
+            Quiz.delay(500).fadeOut(1000);
+            $('#Win3').delay(500).fadeIn(1000);
         }
     })
     $('#Lose a').on('click', function(e){
@@ -39,13 +45,13 @@ $(document).ready(function(){
         $('#Lose').fadeOut(1000);
         $('.Quiz-Start').fadeIn(1000);
     })
-    $('#End a').on('click', function(e){
+    $('#Win2 a, #Win3 a').on('click', function(e){
         e.preventDefault();
         var target1 = $('.select-Btn.btn-success');
         var target2 = $('.select-Btn.btn-danger');
         target1.removeClass('btn-success').addClass('btn-outline-light');;
         target2.removeClass('btn-danger').addClass('btn-outline-light');;
-        $('#End').fadeOut(1000);
+        $('#Win2, #Win3').fadeOut(1000);
         $('.Quiz-Start').fadeIn(1000);
     })
 });
